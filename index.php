2bdff6687651f614c89ff830a3ea223d83ea8484
EC2 Default User <ec2-user@ip-172-31-90-198.ec2.internal>
2020-07-28 13:43:43 +0000

    新規投稿まで

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php
+    // テストプログラム
+    $dsn = 'mysql:host=localhost;dbname=bbs';
+    $username = 'root';
+    $password = '';
+    $messages = array();
+
+    try {
+    
+        $options = array(
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,        // 失敗したら例外を投げる
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,   //デフォルトのフェッチモードはクラス
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',   //MySQL サーバーへの接続時に実行するコマンド
+        ); 
+        
+        $pdo = new PDO($dsn, $username, $password, $options);
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            
+        // PDO::fetch()でカレント1件を取得
+        $stmt = $pdo->query('SELECT * FROM messages');
+        // $stmt->bindParam(':id', $id, PDO::PARAM_STR);
+        $messages = $stmt->fetchAll();
+    } catch (PDOException $e) {
+        echo 'PDO exception: ' . $e->getMessage();
+        exit;
+    }
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+        <title>簡易掲示板</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row mt-2">
+                <h1 class=" col-sm-12 text-center">投稿一覧</h1>
+            </div>
+            <div class="row mt-2">
+                <table class="col-sm-12 table table-bordered table-sprited">
+                    <tr>
+                        <th>ID</th>
+                        <th>ユーザ名</th>
+                        <th>タイトル</th>
+                        <th>内容</th>
+                        <th>投稿時間</th>
+                    </tr>
+                    </tr>
+                <?php foreach($messages as $message){ ?>
+                    <tr>
+                        <td><?php print $message['id']; ?></td>
+                        <td><?php print $message['name']; ?></td>
+                        <td><?php print $message['title']; ?></td>
+                        <td><?php print $message['body']; ?></td>
+                        <td><?php print $message['created_at']; ?></td>
+                    </tr>
+                <?php } ?>
+                </table>
+            </div>
+            <div class="row mt-5">
+                <a href="new.php" class="btn btn-primary">新規投稿</a>
+            </div> 
+        </div>
+        
+        
+        
+        
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>
---
+++ new.php
@@ -0,0 +1,114 @@
+<?php
+    // テストプログラム
+    $dsn = 'mysql:host=localhost;dbname=bbs';
+    $username = 'root';
+    $password = '';
+    $messages = array();
+    $flash_message = "";
+    
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+        
+        $name = $_POST['name'];
+        $title = $_POST['title'];
+        $body = $_POST['body'];
+        
+        try {
+    
+            $options = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,        // 失敗したら例外を投げる
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_CLASS,   //デフォルトのフェッチモードはクラス
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',   //MySQL サーバーへの接続時に実行するコマンド
+            ); 
+            
+            $pdo = new PDO($dsn, $username, $password, $options);
+            $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+                
+            // PDO::fetch()でカレント1件を取得
+            $stmt = $pdo -> prepare("INSERT INTO messages (name, title, body) VALUES (:name, :title, :body)");
+            $stmt->bindParam(':name', $name, PDO::PARAM_STR);
+            $stmt->bindParam(':title', $title, PDO::PARAM_STR);
+            $stmt->bindValue(':body', $body, PDO::PARAM_STR);
+            $stmt->execute();
+            $flash_message = "投稿が成功しました。";
+            
+        } catch (PDOException $e) {
+            echo 'PDO exception: ' . $e->getMessage();
+            exit;
+        }
+    }
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+
+        <title>新規投稿</title>
+        <style>
+            h2{
+                color: red;
+                background-color: pink;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row mt-2">
+                <h1 class="text-center col-sm-12">新規投稿</h1>
+            </div>
+            <div class="row mt-2">
+                <h2 class="text-center col-sm-12"><?php print $flash_message; ?></h1>
+            </div>
+            <div class="row mt-2">
+                <form class="col-sm-12" action="new.php" method="POST">
+                    <!-- 1行 -->
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label">名前</label>
+                        <div class="col-10">
+                            <input type="text" class="form-control" name="name" required>
+                        </div>
+                    </div>
+                
+                    <!-- 1行 -->
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label">タイトル</label>
+                        <div class="col-10">
+                            <input type="text" class="form-control" name="title" required>
+                        </div>
+                    </div>
+                    
+                    <!-- 1行 -->
+                    <div class="form-group row">
+                        <label class="col-2 col-form-label">内容</label>
+                        <div class="col-10">
+                            <input type="text" class="form-control" name="body" required>
+                        </div>
+                    </div>
+                
+                    <!-- 1行 -->
+                    <div class="form-group row">
+                        <div class="offset-2 col-10">
+                            <button type="submit" class="btn btn-primary">投稿</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+             <div class="row mt-5">
+                <a href="index.php" class="btn btn-primary">投稿一覧</a>
+            </div>
+        </div>
+        
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS, then Font Awesome -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    </body>
+</html>

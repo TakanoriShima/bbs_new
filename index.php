4f5450592261a2645fcaf98b0c0f35ba56b45c4c
EC2 Default User <ec2-user@ip-172-31-90-198.ec2.internal>
2020-08-01 10:00:57 +0000

    for Heroku 画像アップロード

--- index.php
+++ index.php
@@ -2,14 +2,14 @@
     
     session_start();
     // テストプログラム
-    // $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_5774074b0e1fbed';
-    // $username = 'be98aadb1041f4';
-    // $password = 'dd672692';
-    // $messages = array();
-    $flash_message = "";
-    $dsn = 'mysql:host=localhost;dbname=bbs';
-    $username = 'root';
-    $password = '';
+    $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_5774074b0e1fbed';
+    $username = 'be98aadb1041f4';
+    $password = 'dd672692';
+    $messages = array();
+    // $flash_message = "";
+    // $dsn = 'mysql:host=localhost;dbname=bbs';
+    // $username = 'root';
+    // $password = '';
     $messages = array();
 
     try {
--- show.php
+++ show.php
@@ -1,17 +1,17 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
         $message_id = $_GET['id'];
-        $dsn = 'mysql:host=localhost;dbname=bbs';
-        $username = 'root';
-        $password = '';
-        $message = "";
+        // $dsn = 'mysql:host=localhost;dbname=bbs';
+        // $username = 'root';
+        // $password = '';
+        // $message = "";
         $image_dir = "upload/";
         
         // テストプログラム
-        // $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_5774074b0e1fbed';
-        // $username = 'be98aadb1041f4';
-        // $password = 'dd672692';
-        // $messages = array();
+        $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_5774074b0e1fbed';
+        $username = 'be98aadb1041f4';
+        $password = 'dd672692';
+        $messages = array();
         $flash_message = "";
     
         try {
@@ -36,16 +36,16 @@
         }
     }else{
         session_start();
-        // $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_5774074b0e1fbed';
-        // $username = 'be98aadb1041f4';
-        // $password = 'dd672692';
+        $dsn = 'mysql:host=us-cdbr-east-02.cleardb.com;dbname=heroku_5774074b0e1fbed';
+        $username = 'be98aadb1041f4';
+        $password = 'dd672692';
         $messages = array();
         $flash_message = "";
         $message_id = $_POST['id'];
         // print $message_id;
-        $dsn = 'mysql:host=localhost;dbname=bbs';
-        $username = 'root';
-        $password = '';
+        // $dsn = 'mysql:host=localhost;dbname=bbs';
+        // $username = 'root';
+        // $password = '';
         
         // $flash_message = null;
     
